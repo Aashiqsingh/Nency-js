7ccf0fe4194fd0281138f4940ee8a4464b17af9c
Aashiqsingh <aashiqsingh56789@gmail.com>
2024-11-11 18:58:39 +0530

    basic , intro

---
+++ Basic/basic1.js
@@ -0,0 +1,39 @@
+// console.log("hello world!");
+// console.warn("warn function");
+// console.error("error function");
+
+
+
+// datatype : number , string , boolean , undefined , object
+
+var a = 10;
+
+console.log("value of a = ",a);
+console.log("type of a = ",typeof(a));
+
+var b = 12.5;
+console.log("value of b = ",b);
+console.log("type of b = ",typeof(b));
+
+var c = "nency";
+console.log("value of c = ",c);
+console.log("type of c = ",typeof(c));
+
+var isMarried = true;
+console.log("value of isMarried = ",isMarried);
+console.log("type of isMarried = ",typeof(isMarried));
+
+var d;
+console.log("value of d = ",d);
+console.log("type of d = ",typeof(d));
+
+var x = null;
+console.log("value of x = ",x);
+console.log("type of x = ",typeof(x));
+
+
+
+
+
+
+
---
+++ Basic/basic2.js
@@ -0,0 +1,68 @@
+// there are three keyword
+// 1. var 
+// 2. let
+// 3. const
+// console.log("value of a = " + a);
+
+// var a = 10;
+// console.log ("value of a = ",a);
+
+// let b = 20;
+// console.log("value of b = ",b);
+
+
+
+// {
+//     console.log("value of a = ",a);
+//     console.log("value of b = ",b);
+    
+    
+// }
+
+// {
+//     var a = 10;
+//     console.log("value of a = ",a);
+
+//     let b = 20;
+//     console.log("value of b = ",b);
+
+
+//     const pi = 3.14;
+//     console.log("value of pi = ",pi);
+    
+    
+// }
+
+// console.log("value of a = ",a);
+// // console.log("value of b = ",b);
+// console.log("value of a = ",pi);
+
+
+// var a = 10;
+// console.log("value of a = ",a);
+
+
+// var a = 20;
+// console.log("value of a = ",a);
+
+// let a = 10;
+// console.log("value of a = ",a);
+
+// a = 20;
+// console.log("value of a = ",a);
+
+const pi = 3.14;
+console.log("value of pi = ",pi);   
+
+pi = 3.14;
+console.log("value of pi = ",pi);
+
+
+
+
+
+
+
+
+
+
